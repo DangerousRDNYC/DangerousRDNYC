efd9e1261ad1700384596a1c068d67b899a3851e
DangerousRDNYC <jarstone@gmail.com>
2013-10-12 17:57:12 -0400

    Create totalfat.php
    PHP for displaying total fatalities

---
+++ totalfat.php
@@ -0,0 +1,90 @@
+<?php
+$q=$_POST["q"];
+$accidentType2=$_POST["accidentType2"];
+
+
+switch ($accidentType2) {
+
+case "BICYCLING":
+$con = mysql_connect('dangerousroadsnyccom.ipowermysql.com','dangerousrd','dangerousrd123');
+
+   if (!$con)
+   {
+     die('Could not connect: ' . mysql_error());
+   }
+
+   mysql_select_db('accidentdata', $con);
+ 
+ $sql = "SELECT sum(Binjured), sum(Bkilled) \n"
+        . "FROM `table 1`\n"
+        . "Where " . $q . "\n";
+   
+       $result = mysql_query($sql);
+
+      while($row = mysql_fetch_array($result))
+      {
+       $injuries = $row['sum(Binjured)'];
+       $killed = $row['sum(Bkilled)'];
+      }
+      mysql_close($con);
+      echo $killed;
+
+break; 
+
+case "DRIVING":
+$con = mysql_connect('dangerousroadsnyccom.ipowermysql.com','dangerousrd','dangerousrd123');
+
+   if (!$con)
+   {
+     die('Could not connect: ' . mysql_error());
+   }
+
+   mysql_select_db('accidentdata', $con);
+ 
+ $sql = "SELECT sum(Cinjured), sum(Ckilled) \n"
+        . "FROM `table 1`\n"
+        . "Where " . $q . "\n";
+   
+       $result = mysql_query($sql);
+
+      while($row = mysql_fetch_array($result))
+      {
+       $injuries = $row['sum(Cinjured)'];
+       $killed = $row['sum(Ckilled)'];
+      }
+      mysql_close($con);
+      echo $killed;
+
+break; 
+
+case "WALKING":
+$con = mysql_connect('dangerousroadsnyccom.ipowermysql.com','dangerousrd','dangerousrd123');
+
+   if (!$con)
+   {
+     die('Could not connect: ' . mysql_error());
+   }
+
+   mysql_select_db('accidentdata', $con);
+ 
+ $sql = "SELECT sum(PedInj), sum(PedKilled) \n"
+        . "FROM `table 1`\n"
+        . "Where " . $q . "\n";
+   
+       $result = mysql_query($sql);
+
+      while($row = mysql_fetch_array($result))
+      {
+       $injuries = $row['sum(PedInj)'];
+       $killed = $row['sum(PedKilled)'];
+      }
+      mysql_close($con);
+      echo $killed;
+break; 
+
+}
+ 
+?>
+
+ 
+  
